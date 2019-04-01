5aa2793ee3a5bbbd9d40644b234a62869702d728
moonishteapot <cole.d.vick@gmail.com>
2019-03-31 22:29:18 -0400

    stacker

---
+++ stacker/stacker.rkt
@@ -0,0 +1,12 @@
+#lang br/quicklang
+42
+
+; (list)
+; 4 => (list 4)
+; 8 => (list 8 4)
+; + => (list + 8 4)
+; 3 => (list 3 12)
+; * => (list * 3 12)
+; 36
+
+; every program specifies a reader and an expander
