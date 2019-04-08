b21df8698e492ea5d2930f92bd8bc3d67df3bb7c
moonishteapot <cole.d.vick@gmail.com>
2019-04-07 23:35:50 -0400

    func expander

--- bf/expander.rkt
+++ bf/expander.rkt
@@ -6,14 +6,9 @@ The pattern (on the right side) of each produc­tion rule describes the possible
 Going to implement an imperative then a functional version
 |#
 
-(define-macro (bf-module-begin PARSE-TREE)
-  #'(#%module-begin
-     PARSE-TREE))
-(provide (rename-out [bf-module-begin #%module-begin]))
-
 ; Begin imperative
 ; each syntax rule from the grammar will get a macro
-
+#|
 ; bf-program : (bf-op | bf-loop)* 
 (define-macro (bf-program OP-OR-LOOP-ARG ...)
   #'(void OP-OR-LOOP-ARG ...))
@@ -48,3 +43,69 @@ Going to implement an imperative then a functional version
 (define (minus) (set-current-byte! (sub1 (current-byte))))
 (define (period) (write-byte (current-byte)))
 (define (comma) (set-current-byte! (read-byte)))
+|#
+
+; begin functional expander
+
+(define-macro (bf-module-begin PARSE-TREE)
+  #'(#%module-begin
+     PARSE-TREE))
+(provide (rename-out [bf-module-begin #%module-begin]))
+
+; apl is a list of a bf array and a pointer, return val of bf func
+; list of bf-funcs
+(define (fold-funcs apl bf-funcs)
+  (for/fold ([current-apl apl])
+            ([bf-func (in-list bf-funcs)])
+    (apply bf-func current-apl)))
+
+(define-macro (bf-program OP-OR-LOOP-ARG ...)
+  #'(begin
+      (define first-apl (list (make-vector 30000 0) 0))
+      (void (fold-funcs first-apl (list OP-OR-LOOP-ARG ...)))))
+(provide bf-program)
+
+(define-macro (bf-loop "[" OP-OR-LOOP-ARG ... "]")
+  #'(lambda (arr ptr)
+      (for/fold ([current-apl (list arr ptr)])
+                ([i (in-naturals)]
+                 #:break (zero? (apply current-byte
+                                       current-apl)))
+        (fold-funcs current-apl (list OP-OR-LOOP-ARG ...)))))
+(provide bf-loop)
+
+(define-macro-cases bf-op
+  [(bf-op ">") #'gt]
+  [(bf-op "<") #'lt]
+  [(bf-op "+") #'plus]
+  [(bf-op "-") #'minus]
+  [(bf-op ".") #'period]
+  [(bf-op ",") #'comma])
+(provide bf-op)
+
+(define (current-byte arr ptr) (vector-ref arr ptr))
+(define (set-current-byte arr ptr val)
+  (define new-arr (vector-copy arr))
+  (vector-set! new-arr ptr val)
+  new-arr)
+
+(define (gt arr ptr) (list arr (add1 ptr)))
+(define (lt arr ptr) (list arr (sub1 ptr)))
+
+(define (plus arr ptr) (list (set-current-byte (add1 (current-byte arr ptr))) ptr))
+(define (minus arr ptr) (list (set-current-byte (sub1 (current-byte arr ptr))) ptr))
+
+(define (period arr ptr) (write-byte (current-byte)))
+(define (comma arr ptr) (list (set)
+
+
+
+
+
+
+
+
+
+
+
+
