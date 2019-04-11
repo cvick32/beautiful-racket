0c03c88259dcda8635c7583767d802163f961b3f
moonishteapot <cole.d.vick@gmail.com>
2019-04-11 00:35:26 -0400

    finish parser

--- jsonic/test.rkt
+++ jsonic/test.rkt
@@ -1,4 +1,4 @@
-
+#|
 #lang jsonic-demo
 // a line comment
 [
@@ -10,3 +10,18 @@
            'key-2 (even? 3)
            'key-3 (hash 'subkey 21)) $@
 ]
+|#
+#lang br
+(require jsonic/parser jsonic/tokenizer brag/support)
+
+(parse-to-datum (apply-tokenizer-maker make-tokenizer "// line comment\n"))
+(parse-to-datum (apply-tokenizer-maker make-tokenizer "@$ 42 $@"))
+(parse-to-datum (apply-tokenizer-maker make-tokenizer "hi"))
+(parse-to-datum (apply-tokenizer-maker make-tokenizer "hi\n//comment\n@$ 42 $@"))
+; here string example
+(parse-to-datum (apply-tokenizer-maker make-tokenizer #<<DEREK
+"foo"
+// comment
+@$ 42 $@
+DEREK
+))
