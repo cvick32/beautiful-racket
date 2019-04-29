33d07ac1f3db6c1ef4023527175158fa2b77e1af
moonishteapot <cole.d.vick@gmail.com>
2019-04-28 20:52:02 -0400

    parser and some tests

---
+++ basic/parse-only.rkt
@@ -0,0 +1,15 @@
+#lang br/quicklang
+(require "parser.rkt" "tokenizer.rkt")
+
+; language to quickly get the parse-tree for a basic program
+(define (read-syntax path port)
+  (define parse-tree (parse path (make-tokenizer port path)))
+  (strip-bindings
+   #`(module basic-parser-mod basic/parse-only
+       #,parse-tree)))
+(module+ reader (provide read-syntax))
+
+(define-macro (parser-only-mb PARSE-TREE)
+  #'(#%module-begin
+     'PARSE-TREE))
+(provide (rename-out [parser-only-mb #%module-begin]))
